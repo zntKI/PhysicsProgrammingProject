4afbc2763d96d88219c869ae0a32db8f217e7496
zntKI <kaloyanivanov@hotmail.com>
2024-03-27 16:19:16 +0100

    Implemented simple cue rotation behavior

--- GXPEngine/Cue.cs
+++ GXPEngine/Cue.cs
@@ -7,10 +7,37 @@ using System.Threading.Tasks;
 
 public class Cue : Sprite
 {
+    private Vec2 position;
+
+    private Vec2 mousePosition;
+
     public Cue(string filename, bool keepInCache = false, bool addCollider = false) : base(filename, keepInCache, addCollider)
     {
         SetOrigin(width, height / 2);
         SetXY(game.width / 2, game.height / 2);
         SetScaleXY(scale / 6f);
+
+        position = new Vec2(x, y);
+        mousePosition = new Vec2(Input.mouseX, Input.mouseY);
+    }
+
+    void Update()
+    {
+        UpdateMousePosition();
+
+        Rotate();
+    }
+
+    private void UpdateMousePosition()
+    {
+        mousePosition.SetXY(Input.mouseX, Input.mouseY);
+    }
+
+    private void Rotate()
+    {
+        Vec2 vecToMouse = mousePosition - position;
+        float deg = vecToMouse.GetAngleDegrees();
+
+        rotation = deg;
     }
 }
