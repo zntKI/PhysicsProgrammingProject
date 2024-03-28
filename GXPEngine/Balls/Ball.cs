9aa23703da00b7638ed07dec4e99ea99e50368bc
zntKI <kaloyanivanov@hotmail.com>
2024-03-28 14:39:42 +0100

    Added realistic ball properties

--- GXPEngine/Balls/Ball.cs
+++ GXPEngine/Balls/Ball.cs
@@ -7,15 +7,53 @@ using System.Threading.Tasks;
 
 public class Ball : Sprite
 {
-    Vec2 position;
+    protected readonly float radius;
+    protected readonly float mass; //kgs
+    protected readonly float bounciness;
+    protected readonly float friction;
+
+    protected Vec2 oldPosition;
+    protected Vec2 position;
+
+    protected Vec2 velocity;
 
     public Ball(string filename, Vec2 position, bool keepInCache = false, bool addCollider = false) : base(filename, keepInCache, addCollider)
     {
+        mass = 0.17f;
+        bounciness = 0.95f;
+        friction = 0.06f;
+
         SetOrigin(width / 2, height / 2);
         scale /= 6f;
 
+        radius = width / 2;
+
         this.position = position;
         UpdateCoordinates();
+        oldPosition = new Vec2();
+
+        velocity = new Vec2();
+    }
+
+    public void Step()
+    {
+        oldPosition = position;
+
+        Move();
+    }
+
+    protected void Move()
+    {
+        position += velocity;
+
+        CheckForBoundaries();
+        //CheckForBalls();
+
+        UpdateCoordinates();
+    }
+
+    private void CheckForBoundaries()
+    {
     }
 
     private void UpdateCoordinates()
