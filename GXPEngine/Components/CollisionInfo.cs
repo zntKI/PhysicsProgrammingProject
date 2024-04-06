641959871bb2dfef134b1f29b0c80bfc8ce98ead
zntKI <kaloyanivanov@hotmail.com>
2024-04-06 09:01:39 +0200

    Created collision info class

---
+++ GXPEngine/Components/CollisionInfo.cs
@@ -0,0 +1,20 @@
+﻿using GXPEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class CollisionInfo
+{
+    public readonly Vec2 normal;
+    public readonly GameObject other;
+    public readonly float timeOfImpact;
+
+    public CollisionInfo(Vec2 pNormal, GameObject pOther, float pTimeOfImpact)
+    {
+        normal = pNormal;
+        other = pOther;
+        timeOfImpact = pTimeOfImpact;
+    }
+}
